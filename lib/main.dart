e0f8c385933835f5fe12fae203f0fd6d47facc66
pixel <chrissx@chrissx.de>
2022-05-26 09:53:30 +0200

    try to get sound half-working
    i'm not exactly sure what i did anymore
    i did all of this in february i think

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,13 @@
-import 'dart:math';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-double bpmFromSamples(final List<DateTime> samples) {
-  if (samples.length < 2) return 0;
+double? bpmFromSamples(final List<DateTime> samples) {
+  if (samples.length < 2) return null;
   final l = samples.reversed.map((x) => x.millisecondsSinceEpoch).toList();
   var y = l.removeAt(0);
   final ms = l.fold<double>(0, (ms, x) {
@@ -27,7 +26,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       home: const MyHomePage(),
     );
@@ -42,28 +41,104 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+// TODO: ways to config these 2
   int _samples = 10;
+  double _speed = 1 / 1;
   List<DateTime> _taps = [];
+  int _bpm = 120;
+  int _index = 0;
+  bool _running = false;
+  final sound = FlutterSoundPlayer();
+
+  void soundIndexLoop() {
+    if (_running) {
+      // TODO: play sound
+      sound.stopPlayer();
+      sound.startPlayer(
+          fromURI:
+              'file:///Users/chris/Library/Mobile Documents/com~apple~CloudDocs/emf/SAMPLES/drums (einzeln)/donks/UAMEE DONK1 G.wav');
+      setState(() => _index = (_index + 1) % 4);
+    }
+    Future.delayed(
+      Duration(microseconds: (60000000 / _bpm).round()),
+      soundIndexLoop,
+    );
+  }
+
+  _MyHomePageState() {
+    sound.openPlayer();
+    soundIndexLoop();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final bpm = bpmFromSamples(_taps.sublist(max(_taps.length - _samples, 0)));
     return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size(600, 120),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200,
+              child: TextField(
+                controller: TextEditingController.fromValue(
+                  TextEditingValue(text: _bpm.round().toString()),
+                ),
+                style: const TextStyle(
+                    fontSize: 100,
+                    fontFamily: 'monospace',
+                    fontFamilyFallback: [
+                      'Menlo',
+                      'Monaco',
+                      'SF Mono',
+                      'Ubuntu Mono'
+                    ]),
+                keyboardType: TextInputType.number,
+                // TODO: NOTE: T?HING AH
+                onChanged: (bpm) => _bpm = int.tryParse(bpm) ?? _bpm,
+              ),
+            ),
+            const Text('BPM', style: TextStyle(fontSize: 100)),
+          ],
+        ),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'BPM: ${bpm.toStringAsFixed(1)} / ${(bpm / 2).toStringAsFixed(1)}',
-              style: const TextStyle(fontSize: 100),
+            IconButton(
+              onPressed: () => _running = !_running,
+              icon: Text(_index.toString()),
             ),
             ElevatedButton(
-              child: const Text('Reset'),
-              onPressed: () => setState(() => _taps = []),
+              child: const Text('Tap', style: TextStyle(fontSize: 72)),
+              onPressed: () => setState(() {
+                _taps.add(DateTime.now());
+                // TODO: improve
+                _bpm = ((bpmFromSamples(_taps.length < _samples
+                                ? _taps
+                                : _taps.sublist(_taps.length - _samples)) ??
+                            120 / _speed) *
+                        _speed)
+                    .round();
+              }),
             ),
-            ElevatedButton(
-              child: const Text('Tap'),
-              onPressed: () => setState(() => _taps.add(DateTime.now())),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  child: const Text('Reset'),
+                  onPressed: () => setState(() {
+                    _taps = [];
+                    _bpm = 120;
+                  }),
+                ),
+                IconButton(
+                  icon: const Icon(CupertinoIcons.settings),
+                  // TODO:
+                  onPressed: () {},
+                ),
+              ],
             ),
           ],
         ),
